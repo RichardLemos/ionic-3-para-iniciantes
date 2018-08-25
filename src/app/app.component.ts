1d48416b895c694e98b317a83d4cc46a70a5965f
Richard Oliveira <richardlemos@unigranrio.br>
2018-08-25 05:08:39 -0300

    Fazendo o slider abrir apenas uma vez(local storage)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { IntroPage } from '../pages/intro/intro';
 import { ConfigProvider } from '../providers/config/config';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html',
@@ -13,7 +14,7 @@ import { ConfigProvider } from '../providers/config/config';
   ]
 })
 export class MyApp {
-  rootPage: any = IntroPage;
+  rootPage: any;
 
   constructor(
     platform: Platform,
@@ -25,7 +26,14 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
 
-      let config = configProvider.getConfigData;
+      let config = configProvider.getConfigData();
+      if (config == null) {
+        this.rootPage = IntroPage;
+        configProvider.setConfigData(false);
+      } else {
+        this.rootPage = TabsPage;
+      }
+
       console.log(config);
 
       statusBar.styleDefault();
